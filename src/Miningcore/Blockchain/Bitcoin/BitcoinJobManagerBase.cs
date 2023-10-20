e645b216b91bfb79ca8eac08ff9a7e5be683dc70
Decho Kocharin <sourcedecho@outlook.com>
2023-10-20 11:51:57 +0700

    Revert "Modified BechPrefix"
    This reverts commit a19e9893276cc60f3d60268070a2d2cfac785634.

--- src/Miningcore/Blockchain/Bitcoin/BitcoinJobManagerBase.cs
+++ src/Miningcore/Blockchain/Bitcoin/BitcoinJobManagerBase.cs
@@ -536,13 +536,10 @@ public abstract class BitcoinJobManagerBase<TJob> : JobManagerBase<TJob>
         switch(addressType.Value)
         {
             case BitcoinAddressType.BechSegwit:
-                return BitcoinUtils.BechSegwitAddressToDestination(poolConfig.Address, network, extraPoolConfig?.BechPrefix);
+                return BitcoinUtils.BechSegwitAddressToDestination(poolConfig.Address, network);
 
             case BitcoinAddressType.BCash:
                 return BitcoinUtils.BCashAddressToDestination(poolConfig.Address, network);
-
-            case BitcoinAddressType.Litecoin:
-                return BitcoinUtils.LitecoinAddressToDestination(poolConfig.Address, network);
 
             default:
                 return BitcoinUtils.AddressToDestination(poolConfig.Address, network);
